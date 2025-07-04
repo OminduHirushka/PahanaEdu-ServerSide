b1aaa0fb4719343b2a0c39bb057573d048134b75
Omindu Hirushka <102536488+OminduHirushka@users.noreply.github.com>
2025-07-04 22:29:12 +0530

    initial commit

---
+++ src/test/java/com/PahanaEdu/PahanaEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PahanaEdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PahanaEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
